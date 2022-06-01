3fddfdd184501dcf7d2501e9cf30a013a92b2575
Lance Rogan <77311869+lance1ca@users.noreply.github.com>
2022-06-01 16:07:04 -0700

    establishing database connection to pgAdmin PostgresSQL DB

---
+++ database.js
@@ -0,0 +1,32 @@
+//This allows us to access the env file
+require('dotenv').config()
+
+//Creating an object Client from the pg package
+const {Client} = require('pg')
+
+//creating an object with specific parameters
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+client.connect((error) => {
+    if (error) {
+      console.error('connection error', error.stack)
+    } else {
+      console.log('connected')
+    }
+  })
+
+
+client.query(`SELECT * FROM clients`, (error,results)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log(results.rows)
+    }
+    client.end()
+})
