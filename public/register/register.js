0cd22e1a1c4101c25e74aa52fdbbe3b262db8cfb
Lance Rogan <77311869+lance1ca@users.noreply.github.com>
2022-06-07 10:27:44 -0700

    Refactoring front end form validation

--- public/register/register.js
+++ public/register/register.js
@@ -3,9 +3,9 @@ const zxcvbn = require('zxcvbn')
 
 
 
-function validateForm(){   
-    //Could also do validateForm(event), event.preventDefault() to ensure that
-    // if the event is not handled, then the action shouldn't be taken
+function validateForm(event){   
+    event.preventDefault() // stops submit form to get submitted in order to do the checks first
+    
 
 const first_name = document.getElementById('first_name').value;
 const last_name = document.getElementById('last_name').value;
@@ -13,60 +13,50 @@ const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
 const password_confirm = document.getElementById('password_confirm').value;
 
-let isValid = false;
+const passwordCracking = zxcvbn(password)
+const passwordScore = passwordCracking.score
+const passwordSuggestions = passwordCracking.feedback.suggestions
+
 
 
 //Check that all fields are filled out
 if(!validator.isEmpty(first_name) || !validator.isEmpty(last_name) || !validator.isEmpty(email) || !validator.isEmpty(password) || !validator.isEmpty(password_confirm) ){
-    isValid = true
-}else{
-    alert('You are missing fields.')
-    isValid = false
-}
-
-
-//Check that email is valid
+    //Check that email is valid
 
 if(validator.isEmail(email)){
-    document.getElementById('email').value = validator.normlaizeEmail(email)
-    isValid = true
+    document.getElementById('email').value = validator.normalizeEmail(email)
+    
+    //Check that password meets the character,symbol, and number requirements
+    if(validator.isStrongPassword(password)){
+
+        //Check that password passes the score test
+
+    if(passwordScore >=2){
+    
+        //Check that passwords match
+    if(password === password_confirm){
+    
+        //if all of these are true, submit the form, otherwise display an error
+        document.getElementById('register').submit()
+    }else{
+        alert('Passwords do not match')
+    }
+
+
+    }else{
+        alert('Password score was ' + passwordScore+"/5, please try again.")
+    }
+  
+    }else{
+        alert('Password does not meet the requirements.')
+    }   
 }else{
-    alert('Invalid email address.')
-    isValid = false
+    alert('Invalid email address')
 }
-
-//Check that password meets the character,symbol, and number requirements
-if(validator.isLength('password',{min: 8}).isStrongPassword()){
-    isValid = true
 }else{
-    alert('Your password does not meet the requirements.')
-    isValid = false
+    alert('You are missing fields')
 }
 
-//Check that password passes the score test
-const passwordCracking = zxcvbn(password)
-const passwordScore = passwordCracking.score
-const passwordSuggestions = passwordCracking.feedback.suggestions
-if(passwordScore >=2){
-    isValid = true
-}else{
-    alert('Your password scored a ' + passwordScore+"/5, please try again.")
-    isValid = false
-}
-
-//Check that passwords match
-if(password === password_confirm){
-    isValid = true
-}else{
-    alert('Your passwords did not match, try again.')
-    isValid = false
-}
-
-
-
-if(isValid){
-    document.getElementById('register').submit()
-}
 
 }
 
